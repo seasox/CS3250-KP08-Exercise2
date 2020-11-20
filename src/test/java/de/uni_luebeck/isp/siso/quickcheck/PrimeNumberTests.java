721a46df648c6a2dd5adb56ee6362259a04d63be
Jeremy Boy <github@jboy.eu>
2020-11-20 12:06:11 +0100

    Implement primeFactorsShouldBePrimeNumbers

--- src/test/java/de/uni_luebeck/isp/siso/quickcheck/PrimeNumberTests.java
+++ src/test/java/de/uni_luebeck/isp/siso/quickcheck/PrimeNumberTests.java
@@ -3,15 +3,22 @@ package de.uni_luebeck.isp.siso.quickcheck;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
-
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 @RunWith(JUnitQuickcheck.class)
 public class PrimeNumberTests {
     
     @Property
     public void primeFactorsShouldBePrimeNumbers(@InRange(min = "2", max = "999999999") Long a) {
-    	// TODO: Write property-based test
+        for (long number: PrimeCalculator.primeFactorization(a)) {
+            // assertions derived from PrimeCalculator.isPrime. We do not use the actual implementation from
+            // PrimeCalculator to be independent of possible bugs introduced in isPrime
+            assertTrue(number == 2 || number < 2 || number % 2 != 0);
+            for (int i = 3; i * i <= number; i += 2)
+                assertNotEquals(0, number % i);
+        }
     }
 	
     // TODO: Write two more tests.
